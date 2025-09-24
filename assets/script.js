d1d8b1af6c7c478c1074084902038c8f56b65f7e
Patricio Melo <42676797+patriciomelor@users.noreply.github.com>
2025-09-23 22:51:04 -0300

    Actualizar número de WhatsApp en el formulario de contacto

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const nombre = document.getElementById('nombre').value;
     const email = document.getElementById('email').value;
     const mensaje = document.getElementById('mensaje').value;
-    const numeroWhatsapp = '56912345678'; // Reemplaza con tu número de WhatsApp con el código de país.
+    const numeroWhatsapp = '56967092468'; // Reemplaza con tu número de WhatsApp con el código de país.
 
     const textoWhatsapp = `Hola, mi nombre es ${nombre}. Mi correo es ${email}. Mensaje: ${mensaje}`;
     const urlWhatsapp = `https://wa.me/${numeroWhatsapp}?text=${encodeURIComponent(textoWhatsapp)}`;
